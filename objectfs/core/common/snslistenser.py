eb8ef7130b36f66d5a64b70e670c3094e0e7ad1d
Kunal Lillaney <lillaney@jhu.edu>
2019-01-01 19:30:21 -0500

    [core][common] Addins sns listener

---
+++ objectfs/core/common/snslistenser.py
@@ -0,0 +1,41 @@
+from __future__ import print_function, absolute_import
+import json
+from BaseHTTPServer import  HTTPServer, BaseHTTPRequestHandler
+from objectfs.settings import Settings
+settings = Settings()
+import logging
+
+
+class SnsHttpServer(HTTPServer):
+
+    def set_fs(self, file_system):
+        
+        self.fs = file_system
+
+class SnsHttpRequestHandler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        """Get is not supported"""
+        self.send_response(404)
+
+    def do_HEAD(self):
+        """Head is not supported"""
+        self.send_response(404)
+
+    def do_POST(self):
+        """A notification is posted"""
+        
+        import pdb; pdb.set_trace()
+        content_length = self.headers.getheader('content-length')
+        post_body = self.rfile.read(content_length)
+        message_content = json.loads(post_body)
+        # logic to update local memory
+        # self.server.fs
+
+class SnsHttpFactory(object):
+
+    @staticmethod
+    def run_server(file_system, server_class=SnsHttpServer, handler_class=SnsHttpRequestHandler, server_host=settings.SNS_HOST, server_port=settings.SNS_PORT):
+        http_daemon = server_class((server_host, server_port), handler_class)
+        http_daemon.set_fs(file_system)
+        http_daemon.serve_forever()
