0bca86cb050267cce39d15965730866856926e03
Kunal Lillaney <lillaney@jhu.edu>
2019-01-04 00:58:25 -0500

    [core][common] Updating sns and adding threading for objectfs. Also adding license to files.

--- objectfs/core/common/snslistenser.py
+++ objectfs/core/common/snslistenser.py
@@ -1,10 +1,29 @@
+# Copyright 2017 IBM Corporation
+# Copyright 2017 The Johns Hopkins University
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import print_function, absolute_import
 import json
+import boto3
 from BaseHTTPServer import  HTTPServer, BaseHTTPRequestHandler
+from objectfs.core.common.snsqueue import SnsTopic
 from objectfs.settings import Settings
 settings = Settings()
 import logging
 
+# use the 0.0.0.0 address since on EC2 we cannot attach to the public IP address
+HOST = '0.0.0.0'
 
 class SnsHttpServer(HTTPServer):
 
@@ -28,20 +47,36 @@ class SnsHttpRequestHandler(BaseHTTPRequestHandler):
         """A notification is posted"""
 
         print("POST")
-        import pdb; pdb.set_trace()
         content_length = self.headers.getheader('content-length')
-        post_body = self.rfile.read(content_length)
-        message_content = json.loads(post_body)
-	message_type = self.headers.getheader('x-amz-sns-message-type')
-	if message_type == 'SubscriptionConfirmation':
-	    token = message_content['Token']
+        message_body = self.rfile.read(int(content_length))
+        message_content = json.loads(message_body)
+        message_type = self.headers.getheader('x-amz-sns-message-type')
+        if message_type == 'SubscriptionConfirmation':
+            # send confirmation token to SNS
+            token = message_content['Token']
+            sns_topic = SnsTopic()
+            sns_topic.confirm_subscription(token)
+        elif message_type == 'Notification':
+            # import pdb; pdb.set_trace()
+            message = json.loads(message_content['Message'])
+            bucket_name = message['Records'][0]['s3']['bucket']['name']
+            object_key = message['Records'][0]['s3']['object']['key']
+            # process message
+            self.server.fs.process_notification()
+            # send 200 to notification received
+            self.send_response(200)
+        else:
+            # send 400 back if we cannot parse the message
+            self.send_response(400)
+            
         # logic to update local memory
         # self.server.fs
 
 class SnsHttpFactory(object):
 
     @staticmethod
-    def run_server(file_system, server_class=SnsHttpServer, handler_class=SnsHttpRequestHandler, server_host=settings.SNS_HOST, server_port=settings.SNS_PORT):
+    def run_server(file_system, server_class=SnsHttpServer, handler_class=SnsHttpRequestHandler, server_host=HOST, server_port=settings.SNS_PORT):
         http_daemon = server_class((server_host, server_port), handler_class)
         http_daemon.set_fs(file_system)
+        print("Running server")
         http_daemon.serve_forever()
--- objectfs/core/common/snsqueue.py
+++ objectfs/core/common/snsqueue.py
@@ -1,3 +1,19 @@
+# Copyright 2017 IBM Corporation
+# Copyright 2017 The Johns Hopkins University
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function, absolute_import
 import boto3
 from objectfs.settings import Settings
 settings = Settings()
@@ -5,23 +21,37 @@ settings = Settings()
 class SnsConnection(object):
 
     def __init__(self):
-        self.conn = boto3.resource('sns', region_name=settings.S3_AWS_REGION, endpoint_url=settings.S3_ENDPOINT, aws_access_key=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+        self.conn = boto3.client('sns', region_name=settings.S3_AWS_REGION, endpoint_url=settings.S3_ENDPOINT, aws_access_key=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
 
 
 class SnsTopic(object):
 
-    def __init__(self, topic_name):
+    def __init__(self):
+        self.conn = boto3.client('sns', region_name=settings.S3_AWS_REGION, endpoint_url=settings.S3_ENDPOINT, aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
         sns = boto3.resource('sns', region_name=settings.S3_AWS_REGION, endpoint_url=settings.S3_ENDPOINT, aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-        self.topic = sns.Topic(topic_name)
+        self.topic_name = settings.SNS_TOPIC_NAME
+        self.topic = sns.Topic(self.topic_name)
         self.endpoint = 'http://{}:{}'.format(settings.SNS_HOST, settings.SNS_PORT)
+    
+    def create(self):
+        """Create topic"""
+        response = self.conn.create_topic(
+                    Name = self.topic_name,
+                    Attributes={
+                      'DisplayName': self.topic_name
+                    }
+                  )
+        return response
 
     def subscribe(self):
         """Subscribe to SNS topic"""
-
         response = self.topic.subscribe(Protocol='http', Endpoint=self.endpoint)
-        response = self.topic.confirm_subscription(Token=response)
+    
+    def confirm_subscription(self, token):
+        """Confirm topic subsscription"""
+        response = self.topic.confirm_subscription(Token=token)
+        return response
 
     def delete(self):
         """Delete the topic"""
-
         response = self.topic.delete()
